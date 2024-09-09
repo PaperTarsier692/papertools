624a6339dfab9fcf91af0bb3be440a136f1dfe44
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-09 21:28:50 +0200

    Update setup.py with required dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     packages=find_packages(),
     author='PaperTarsier692',
     url='https://www.github.com/PaperTarsier692/papertools',
-    long_description='Small collection of various Python tools.', long_description_content_type='text/markdown'
+    long_description='Small collection of various Python tools.', long_description_content_type='text/markdown',
+    requires=[
+        'Groq>=0.11.0'
+    ]
 )
