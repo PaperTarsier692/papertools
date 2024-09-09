a166f5a248af7c4b30831b65683387aaf7ac0f8b
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-09 22:03:54 +0200

    Removed dependency in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,5 @@ setup(
     packages=find_packages(),
     author='PaperTarsier692',
     url='https://www.github.com/PaperTarsier692/papertools',
-    long_description='Small collection of various Python tools.', long_description_content_type='text/markdown',
-    install_requires=[
-        'Groq>=0.11.0'
-    ]
+    long_description='Small collection of various Python tools.', long_description_content_type='text/markdown'
 )
