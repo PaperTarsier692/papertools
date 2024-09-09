39a6fec0c75404dcb4861aadf9928458fa04a96f
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-09 21:58:30 +0200

    Changed Version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='papertools',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     author='PaperTarsier692',
     url='https://www.github.com/PaperTarsier692/papertools',
