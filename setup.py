7484b1155fb1d4ab554b4fcf711855b4039a0a02
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-09 22:00:51 +0200

    Fixed wrong install requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='PaperTarsier692',
     url='https://www.github.com/PaperTarsier692/papertools',
     long_description='Small collection of various Python tools.', long_description_content_type='text/markdown',
-    requires=[
+    install_requires=[
         'Groq>=0.11.0'
     ]
 )
