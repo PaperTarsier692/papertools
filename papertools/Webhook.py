2e75172c3bcc21ddd8aa1ab4653ffb568a582854
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-11 14:12:27 +0200

    Fixed missing msg attribute of webhook.send

--- papertools/Webhook.py
+++ papertools/Webhook.py
@@ -10,7 +10,7 @@ class Webhook:
         self.name: Union[str, None] = name
         self.image: Union[str, None] = image
 
-    def send(self, name: Union[str, None] = None, image: Union[str, None] = None) -> requests.Response:
+    def send(self, msg: str, name: Union[str, None] = None, image: Union[str, None] = None) -> requests.Response:
         '''Sends a message to the webhook'''
         if name == None:
             name = self.name
@@ -18,6 +18,7 @@ class Webhook:
             image = self.image
 
         data = {
+            'content': msg,
             'username': name,
             'avatar_url': image
         }
