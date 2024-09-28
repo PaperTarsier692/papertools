508692090c346cda616b89eae57fa30638faf08e
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-09-28 18:02:32 +0200

    Disables the Groq module if the Groq package isnt installed

--- papertools/__init__.py
+++ papertools/__init__.py
@@ -5,6 +5,9 @@ from .WebServify import WebServify
 from .Cfg import Cfg
 from .Encasings import Encasings
 from .Console import Console
-from .Groq import Groq
+try:
+    from .Groq import Groq
+except ModuleNotFoundError:
+    pass
 from .Ollama import Ollama
 from .Webhook import Webhook
