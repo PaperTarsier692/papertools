fa87f5c4244b17415859978b4e66ff175bd042d7
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-21 14:16:38 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='papertools',
-    version='1.5',
+    version='1.6',
     packages=find_packages(),
     author='PaperTarsier692',
     url='https://www.github.com/PaperTarsier692/papertools',
